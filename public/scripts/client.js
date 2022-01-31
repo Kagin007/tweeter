19f5af63ac0a593fc37e6a6426958631bd0df359
Adam Schulte <adamschulte148@hotmail.com>
2022-01-31 21:54:02 +0000

    added tweeter card body

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,29 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const toggleBackTpTopBtn = () => {
+  if ($(window).scrollTop() > 0) {
+    $(".back-to-top")
+      .show()
+      .fadeIn("slow");
+  } else {
+    $(".back-to-top")
+      .hide()
+      .fadeOut("slow");
+  }
+};
+
+$(document).ready(function() {
+  // hide back to top button at initial render
+  $(".back-to-top").hide();
+  // call toggleBackTpTopBtn on page scroll
+  $(window).scroll(toggleBackTpTopBtn);
+  // clicking on back to top
+  $(".back-to-top").on("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    toggleBackTpTopBtn();
+  });
+});
