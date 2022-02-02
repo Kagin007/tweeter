8b1339b0788ac4bcf9aa1ca131749084ffed1959
Adam Schulte <adamschulte148@hotmail.com>
2022-02-02 14:58:19 +0000

    adding multiple tweets with a for loop over the 'database'

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,34 +17,58 @@
 // };
 
 $(document).ready(function() {
-  // // hide back to top button at initial render
-  // $(".back-to-top").hide();
-  // // call toggleBackTpTopBtn on page scroll
-  // $(window).scroll(toggleBackTpTopBtn);
-  // // clicking on back to top
-  // $(".back-to-top").on("click", () => {
-  //   window.scrollTo({
-  //     top: 0,
-  //     behavior: "smooth"
-  //   });
-  //   toggleBackTpTopBtn();
-  // });
+// // hide back to top button at initial render
+// $(".back-to-top").hide();
+// // call toggleBackTpTopBtn on page scroll
+// $(window).scroll(toggleBackTpTopBtn);
+// // clicking on back to top
+// $(".back-to-top").on("click", () => {
+//   window.scrollTo({
+//     top: 0,
+//     behavior: "smooth"
+//   });
+//   toggleBackTpTopBtn();
+// });
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
- }
+const tweetData = [
+  {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+},
+{
+  "user": {
+    "name": "Descartes",
+    "avatars": "https://i.imgur.com/nlhLi3I.png",
+    "handle": "@rd" },
+  "content": {
+    "text": "Je pense , donc je suis"
+  },
+  "created_at": 1461113959088
+}
+]
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  for (let tweet of tweets) {
+    // calls createTweetElement for each tweet
+    const $tweet = createTweetElement(tweet);
+    // takes return value and appends it to the tweets container
+    $('#tweets-container').append($tweet);    
+  }
+
+}
 
 const createTweetElement = function (data) {
-  const $tweet = $(`
-    <header>
+const $tweet = $(`
+  <article>
+  <header>
     <div id='face-icon-and-name'>
       <img src=${data.user.avatars}>
       <span>${data.user.name}</span>
@@ -53,23 +77,27 @@ const createTweetElement = function (data) {
   </header>
   <p id=tweet>${data.content.text}</p>
   <footer>
-    <span>${data.created_at}</span>
+    <span>
+      ${timeago.format(data.created_at)}
+    </span>
     <icons>
       <i class="fas fa-flag fa-lg"></i>
       <i class="fas fa-retweet fa-lg"></i>
       <i class="fas fa-heart fa-lg"></i>
     </icons>
   </footer>
+  </article>
 `)
-  return $tweet
+return $tweet
 }
 
-const $tweet = createTweetElement(tweetData);
+renderTweets(tweetData)
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet);
-    
+$("form").submit(function(event) {
+  alert("Handler for .subit() called.");
+  event.preventDefault();
+})
+  
 });
 
 
