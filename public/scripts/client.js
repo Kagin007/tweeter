6d1365840d9bca92f7312b258cf8784843d91166
Adam Schulte <adamschulte148@hotmail.com>
2022-02-03 20:24:59 +0000

    fixing eslint and redundant comments

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -17,38 +17,38 @@ const toggleBackTpTopBtn = () => {
 };
 
 $(document).ready(function() {
-// hide back to top button at initial render
-$(".back-to-top").hide();
-// call toggleBackTpTopBtn on page scroll
-$(window).scroll(toggleBackTpTopBtn);
-// clicking on back to top
-$(".back-to-top").on("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
+  // hide back to top button at initial render
+  $(".back-to-top").hide();
+  // call toggleBackTpTopBtn on page scroll
+  $(window).scroll(toggleBackTpTopBtn);
+  // clicking on back to top
+  $(".back-to-top").on("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    toggleBackTpTopBtn();
   });
-  toggleBackTpTopBtn();
-});
 
-const renderTweets = function(tweets) {
-  // loops through tweets
-  for (let tweet of tweets.reverse()) {
-    // calls createTweetElement for each tweet
-    const $tweet = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
-    $('#tweets-container').append($tweet);    
+  const renderTweets = function(tweets) {
+    // loops through tweets
+    for (let tweet of tweets.reverse()) {
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append($tweet);
+    }
   };
-};
 
-//escape function to prevent XSS (cross site scripting)
-const escape = function (str) {
-  let div = document.createElement("div");
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
+  //escape function to prevent XSS (cross site scripting)
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-const createTweetElement = function (data) {
-const $tweet = $(`
+  const createTweetElement = function(data) {
+    const $tweet = $(`
   <article>
   <header>
     <div id='face-icon-and-name'>
@@ -69,47 +69,47 @@ const $tweet = $(`
     </icons>
   </footer>
   </article>
-`)
-return $tweet
-}
-
-$("form").submit(function(event) {
-  event.preventDefault();
-  const charCounter = Number($('#char-counter').val())  
-  
-  if (charCounter < 0) {
-    $('.errorLength').slideDown()
-    $('.errorNoText').slideUp();
-  } else if (charCounter === 140) {
-    $('.errorLength').slideUp();
-    $('.errorNoText').slideDown()
-  } else {
-    $('.errorLength').slideUp();
-    $('.errorNoText').slideUp();
-    const serializedForm = $(this).serialize();
-    $('#tweet-text').val("") 
-    
-    //post to database
-    $.post('/tweets', serializedForm )
-      .done(function () {
-        $('#tweets-container').empty(); //causes bumping on submit
-        //reset counter to 140
-        $('#char-counter').val(140)
-
-        //load tweets
-        loadTweets()
-    })
+`);
+    return $tweet;
   };
-});
 
-const loadTweets = function () {
-  $.ajax('/tweets', {method: 'GET'})
-  .then(function (data) {
-    renderTweets(data)
-  })
-}
+  $("form").submit(function(event) {
+    event.preventDefault();
+    const charCounter = Number($('#char-counter').val());
+  
+    if (charCounter < 0) {
+      $('.errorLength').slideDown();
+      $('.errorNoText').slideUp();
+    } else if (charCounter === 140) {
+      $('.errorLength').slideUp();
+      $('.errorNoText').slideDown();
+    } else {
+      $('.errorLength').slideUp();
+      $('.errorNoText').slideUp();
+      const serializedForm = $(this).serialize();
+      $('#tweet-text').val("");
+    
+      //post to database
+      $.post('/tweets', serializedForm)
+        .done(function() {
+          $('#tweets-container').empty(); //causes bumping on submit
+          //reset counter to 140
+          $('#char-counter').val(140);
 
-loadTweets()
+          //load tweets
+          loadTweets();
+        });
+    }
+  });
+
+  const loadTweets = function() {
+    $.ajax('/tweets', {method: 'GET'})
+      .then(function(data) {
+        renderTweets(data);
+      });
+  };
+
+  loadTweets();
 
 });
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
   $("#tweet-text").on("input", function() {
-    const charCounter = ($(`#${this.id}`).parent().next().find('.counter'))
+    const charCounter = ($(`#${this.id}`).parent().next().find('.counter'));
 
     let currentCounter = $(charCounter).val();
     if (currentCounter <= -1 || currentCounter === 0) {
-      $(charCounter).addClass('red-text')
+      $(charCounter).addClass('red-text');
     } else {
-      $(charCounter).removeClass('red-text')
+      $(charCounter).removeClass('red-text');
     }
 
-    $(charCounter).val( 140 - this.value.length);
-  })
+    $(charCounter).val(140 - this.value.length);
+  });
 });
